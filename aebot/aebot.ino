2ae842ec39316de1a196d6ffd9353c900a98cde5
Joseph DeVictoria <jldevictoria@gmail.com>
2014-01-24 12:49:27 -0700

    This is the base file, I will edit it to get it to spec

---
+++ aebot/aebot.ino
@@ -0,0 +1,21 @@
+void setup() {
+  // This is where we set up the transmit type for each pin:
+  // pinMode(0, INPUT/OUTPUT); //Digital Pin / Touch / RX1
+  // pinMode(1, INPUT/OUTPUT); //Digital Pin / Touch / RX1
+  // pinMode(2, INPUT/OUTPUT); //Digital Pin / CS
+  // pinMode(3, INPUT/OUTPUT); //Digital Pin / PWM
+  // pinMode(4, INPUT/OUTPUT);
+  // pinMode(5, INPUT/OUTPUT);
+  // pinMode(6, INPUT/OUTPUT);
+  // pinMode(7, INPUT/OUTPUT);
+  // pinMode(8, INPUT/OUTPUT);
+  // pinMode(9, INPUT/OUTPUT);
+  // pinMode(10, INPUT/OUTPUT);
+  // pinMode(11, INPUT/OUTPUT);
+  // pinMode(12, INPUT/OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+  //digitalWrite(3, 1);
+}
